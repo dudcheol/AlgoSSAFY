2659781ed7835cad8167ad2e0c7acf0d6fd7560b
dudcheol <95_pyc@naver.com>
2020-08-01 16:35:57 +0900

    프로그래머스 / 레벨3 / 보석 쇼핑 - 정답

--- 1주차-2/보석쇼핑/박영철/JewelryShopping.java
+++ 1주차-2/보석쇼핑/박영철/JewelryShopping.java
@@ -2,13 +2,13 @@ package week1;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class JewelryShopping {
 
     static int[] solution(String[] gems) {
         int size = Integer.MAX_VALUE;
         int answerS = 0, answerE = 0;
+        /* kind : 보석의 종류 */int kind = 1;
 
         // gems size 1~100,000
         // 종류 찾기
@@ -25,16 +25,21 @@ public class JewelryShopping {
         map.put(gems[start], 1);
 
         while (true) {
-            boolean flag = map.containsValue(0);
+//            boolean hasAllKind = !map.containsValue(0);
+            /* Hashmap.containsValue = O(N)
+            * 최악의 경우 N = 100,000 이고 보석의 종류가 100,000 이라면
+            * 매 반복문마다 map에서 100,000번의 연산이 수행됨 */
+            boolean hasAllKind = map.size() == kind;
 
-            if (map.size() == size || (end == gems.length - 1 && flag)) break;
+            if (map.size() == size || (end == gems.length - 1 && !hasAllKind)) break;
 
-            if (flag) {
+            if (!hasAllKind) {
                 // 아직 모든 종류를 사지 않았다면
                 // end 위치의 보석을 확인하여 갯수 추가
                 end++;
                 if (end > gems.length - 1 || start > end) break;
                 map.put(gems[end], map.get(gems[end]) + 1);
+                if (map.get(gems[end]) == 1) kind++;
                 continue;
             }
 
@@ -50,6 +55,7 @@ public class JewelryShopping {
 
             // 모든 종류가 있는 상태이므로, end를 늘리지 않고 start를 늘린다
             map.put(gems[start], map.get(gems[start]) - 1);
+            if (map.get(gems[start]) == 0) kind--;
             start++;
         }
 
@@ -58,9 +64,11 @@ public class JewelryShopping {
 
     public static void main(String[] args) {
 //        String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
-        String[] gems2 = {"AA", "AB", "AC", "AA", "AC"};
+//        String[] gems2 = {"AA", "AB", "AC", "AA", "AC"};
+        String[] gems3 = {"ZZZ", "YYY", "NNNN", "YYY", "BBB"};
 
 //        System.out.println(Arrays.toString(solution(gems)));
-        System.out.println(Arrays.toString(solution(gems2)));
+//        System.out.println(Arrays.toString(solution(gems2)));
+        System.out.println(Arrays.toString(solution(gems3)));
     }
 }
