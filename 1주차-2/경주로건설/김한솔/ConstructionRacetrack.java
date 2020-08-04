6329641d32870af459d40864844eb033d06206ad
dudcheol <95_pyc@naver.com>
2020-08-04 19:29:59 +0900

    [ 병합 ]

--- 1주차-2/경주로건설/김한솔/ConstructionRacetrack.java
+++ 1주차-2/경주로건설/김한솔/ConstructionRacetrack.java
@@ -1,16 +1,24 @@
-package kakao;
+package programmers;
 
-class ConstructionRacetrack {
+class ConstructionRaceTrack {
 	
 	private static int[][] cost;
-	private static int[][] boardArr;
-	private static int dx[] = {0, 1, 0, -1};
+	private static int[][] track;
+	private static int dx[] = {0, 1, 0, -1}; // 우, 하, 좌, 상
 	private static int dy[] = {1, 0, -1, 0};
+		
+	public static void main(String[] args) {
+		
+		int [][] board = {{0,0,0,0,0,0},{0,1,1,1,1,0},{0,0,1,0,0,0},
+				{1,0,0,1,0,1},{0,1,0,0,0,1},{0,0,0,0,0,0}};
+		
+		solution(board);
+	}
 	
-    public int solution(int[][] board) {
+    public static int solution(int[][] board) {
         int answer = 0;
         cost = new int [board.length][board.length];
-        boardArr = board;
+        track = board;
         
         for(int i=0; i<board.length; i++) {
             for(int j=0; j<board.length; j++) {
@@ -20,8 +28,15 @@ class ConstructionRacetrack {
         
         cost[0][0] = 0;
         
-        dfs(0, 0, 0);
-        dfs(0, 0, 1);
+        dfs(0, 0, 0); // 우측 탐색 시작
+        dfs(0, 0, 1); // 아랫쪽 탐색 시작
+        
+/*	    for(int i=0; i<board.length; i++) {
+	    	for(int j=0; j<board.length; j++) {
+	    		System.out.printf("%-11d", cost[i][j]);
+	    	}
+	    	System.out.println();
+	    }*/
         
         answer = cost[board.length-1][board.length-1];
        
@@ -29,7 +44,7 @@ class ConstructionRacetrack {
     }
     
     
-    public void dfs(int x, int y, int direction) {     	    
+    public static void dfs(int x, int y, int direction) {     	    
     	
     	if(x == cost.length-1 && y == cost.length-1) {
     		return;
@@ -40,19 +55,24 @@ class ConstructionRacetrack {
     		int rx = x+dx[i];
     		int ry = y+dy[i];
     		
-    		if(rx < boardArr.length && ry < boardArr.length && rx >= 0 && ry >= 0) { 
-    			if(boardArr[rx][ry] == 0) {
+    		if(isInTrack(rx, ry) && !isWall(rx, ry)) { // 경주로 트랙 안에 존재하고 벽이 아닐때
     				
-    				int a = cost[rx][ry];
-    				int b = (i%2 == direction) ? cost[x][y]+100 : cost[x][y]+600;
-    				
-    				if(a >= b) {
-						cost[rx][ry] = b;
-						dfs(rx, ry, i%2);
-					}    				
-    			}
+				int a = cost[rx][ry];
+				int b = (i%2 == direction) ? cost[x][y]+100 : cost[x][y]+600;
+				
+				if(a >= b) {
+					cost[rx][ry] = b;
+					dfs(rx, ry, i%2);
+				}    				
     		}
     	}
-
+    }
+    
+    private static boolean isInTrack(int x, int y) {
+    	return (x < track.length && y < track.length && x >= 0 && y >= 0) ? true : false;
+    }
+    
+    private static boolean isWall(int x, int y) {
+    	return (track[x][y] == 1) ? true : false;
     }
 }
