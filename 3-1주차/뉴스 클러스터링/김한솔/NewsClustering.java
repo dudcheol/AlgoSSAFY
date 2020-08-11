0ccf32a797cc034bfee063d98c46b4d68cc524c5
mentalK94 <31653025+mentalK94@users.noreply.github.com>
2020-08-11 15:00:38 +0900

    Add files via upload

---
+++ 3-1주차/뉴스 클러스터링/김한솔/NewsClustering.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.ArrayList;
+
+public class NewsClustering {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        ArrayList<String> str1Arr = new ArrayList<>();
+        ArrayList<String> str2Arr = new ArrayList<>();
+        
+        for(int i=0; i<str1.length()-1; i++) {
+        	if((str1.charAt(i) >= 48 && str1.charAt(i) <= 57) ||
+        		(str1.charAt(i) >= 65 && str1.charAt(i) <= 90) ||
+        		(str1.charAt(i) >= 97 && str1.charAt(i) <= 122)) {
+        		
+        	}
+        }
+        
+        for(int i=0; i<str2.length(); i++) {
+        	
+        }
+        
+        
+        return answer;
+    }
+}
