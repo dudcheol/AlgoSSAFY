0ba7a551a5e44cbc4963dbff0e8e7dac9cd927c9
cheonghwakim <43662673+cheonghwakim@users.noreply.github.com>
2020-08-19 00:47:54 +0900

    Add files via upload

--- 4-1주차/H-Index/H_Index.java
+++ 4-1주차/H-Index/H_Index.java
@@ -3,7 +3,9 @@ package programmers;
 import java.util.Arrays;
 
 public class H_Index {
-	static int[] citations = { 5, 5, 5, 5 };
+	//static int[] citations = { 6, 6, 6, 6, 6, 1 };
+	//static int[] citations = { 20, 21, 22, 23 };
+	static int[] citations = { 4, 4, 4 };
 	
 	public static void main(String[] args) {
 		int answer = 0;
@@ -20,9 +22,10 @@ public class H_Index {
 			high = n - i; // 5 - 0 = 5
 			
 			// h번 이상 인용된 논문 h 이상
-			if (high >= h) {
-				answer = h;
+			if (high >= h) { // 이 순간 최대가 될 수 없음
+				break;
 			}
+			answer++; // 내림차순으로 탐색했기 때문에 가장 큰 h가 answer++되고 최종
 		}
 	        
 		System.out.println(answer);
