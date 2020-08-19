bb6aedb3829349de1eeea100879680f1e4a213b5
cheonghwakim <43662673+cheonghwakim@users.noreply.github.com>
2020-08-19 09:30:09 +0900

    Add files via upload

--- 4-1주차/가장 큰 수/LargestNumber_Comparator.java
+++ 4-1주차/가장 큰 수/LargestNumber_Comparator.java
@@ -10,28 +10,28 @@ public class LargestNumber_Comparator {
 	public static void main(String[] args) {
         String answer = "";
         
-        //int 배열을 String 배열로 변환
         String[] arr = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
             arr[i] = (String.valueOf(numbers[i]));
         }
         
-        //배열 정렬, 정렬 규칙으로는 2개를 더하여 더 큰 쪽이 우선순위가 있도록 정렬
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return (s2 + s1).compareTo(s1 + s2);
-            }
-        });
+        // Comparator: 일반적이지 않은 기준으로 정렬, 2개를 더하여 더 큰 쪽이 우선순위가 있도록 정렬
+        Comparator<String> comparator = new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return (o2 + o1).compareTo(o1 + o2); // 내림차순
+			}      	
+        };
         
-        //0000 처럼 0으로만 구성되어있으면 0 return
-        if (arr[0].equals("0")) 
-        	answer = "0";
+        Arrays.sort(arr, comparator);
         
-        //그 외의 경우 순차적으로 연결하여 answer return
         for (int i = 0; i < arr.length; i++) {
             answer += arr[i];
-        }
+        }       
+        
+        // 정렬 후 0으로 시작할 경우 -> 0 return
+        if (arr[0].equals("0")) 
+        	answer = "0";
         
         System.out.println(answer);
 	}
