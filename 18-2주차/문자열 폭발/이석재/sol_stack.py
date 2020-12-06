e2909185426afd1ffaf49a55055937bcd3bf3320
doljae <37795866+doljae@users.noreply.github.com>
2020-12-06 11:29:25 +0900

    Create sol_stack.py

---
+++ 18-2주차/문자열 폭발/이석재/sol_stack.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+r = sys.stdin.readline
+
+target_str, pattern_str = r().rstrip(), r().rstrip()
+
+target_list, pattern_length = deque(list(target_str)), len(pattern_str)
+stack = []
+stack_length = len(stack)
+while target_list:
+    stack.append(target_list.popleft())
+    stack_length += 1
+    if stack_length >= pattern_length:
+        if "".join(stack[stack_length - pattern_length:]) == pattern_str:
+            count = 0
+            while count < pattern_length:
+                stack.pop()
+                count += 1
+                stack_length -= 1
+print("".join(stack) if stack else "FRULA")
