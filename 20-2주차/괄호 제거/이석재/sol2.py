69060f215dc0a06374ce2e2ccf115358a143222c
doljae <37795866+doljae@users.noreply.github.com>
2020-12-19 20:23:08 +0900

    Update sol2.py

--- 20-2주차/괄호 제거/이석재/sol2.py
+++ 20-2주차/괄호 제거/이석재/sol2.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 from itertools import combinations
 
 r = sys.stdin.readline
@@ -8,6 +7,7 @@ input_string = r().rstrip()
 list1, list2 = [], []
 input_list = list(input_string)
 
+# 유정이 코드 보고 한번에 처리해줌
 stack = []
 brackets = []
 for index, value in enumerate(input_list):
@@ -16,20 +16,9 @@ for index, value in enumerate(input_list):
     elif value == ")":
         brackets.append((stack.pop(), index))
 
-# 유정이 코드 보고 한번에 처리해줌
-# while list1:
-#     cur = list1.pop()
-#     temp = []
-#     while cur > list2[0]:
-#         temp.append(list2.popleft())
-#     brackets.append((cur, list2[0]))
-#     list2.popleft()
-#     list2 = deque(temp + list(list2))
-
 result = []
 for i in range(1, len(brackets) + 1):
     for item in combinations(brackets, i):
-        # print(item)
         temp = []
         for item2 in item:
             temp.append(item2[0])
