a176661714453e7da799f9c3eeda4d2d8404edc4
doljae <37795866+doljae@users.noreply.github.com>
2020-12-27 01:24:20 +0900

    Create sol_python.py

---
+++ 21-2주차/집합의 표현/이석재/sol_python.py
@@ -0,0 +1,37 @@
+import sys
+
+r = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
+
+node_num, command_length = map(int, r().split())
+
+parents = [i for i in range(0, node_num + 1)]
+rank = [0] * (node_num + 1)
+
+
+def find(node):
+    if node == parents[node]:
+        return node
+    else:
+        parents[node] = find(parents[node])
+        return parents[node]
+
+
+def union(node1, node2):
+    parent1, parent2 = find(node1), find(node2)
+
+    if parent1 != parent2:
+        if rank[parent1] > rank[parent2]:
+            parents[parent2] = parent1
+        else:
+            parents[parent1] = parent2
+            if rank[parent1] == rank[parent2]:
+                rank[parent2] += 1
+
+
+for _ in range(command_length):
+    flag, n1, n2 = map(int, r().split())
+    if flag == 0:
+        union(n1, n2)
+    elif flag == 1:
+        print("NO" if find(n1) != find(n2) else "YES")
