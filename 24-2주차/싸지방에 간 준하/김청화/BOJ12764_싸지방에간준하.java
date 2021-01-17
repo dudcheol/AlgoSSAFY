3ecf685dcde11f736bca38d9f88de6c048eaa3f7
cheonghwakim <43662673+cheonghwakim@users.noreply.github.com>
2021-01-17 09:54:42 +0900

    Add files via upload

--- 24-2주차/싸지방에 간 준하/김청화/BOJ12764_싸지방에간준하.java
+++ 24-2주차/싸지방에 간 준하/김청화/BOJ12764_싸지방에간준하.java
@@ -3,6 +3,7 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -10,13 +11,22 @@ import java.util.StringTokenizer;
 public class BOJ12764_싸지방에간준하 {
 	static int N;
 	static int[] computer = new int[1000001];
-//	static ArrayList<int[]> list = new ArrayList<>();
+	static int[][] list;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		
-		PriorityQueue<int[]> list = new PriorityQueue<int[]>(new Comparator<int[]>() {
+		list = new int[N][2];
+		StringTokenizer st = null;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			list[i][0] = Integer.parseInt(st.nextToken());
+			list[i][1] = Integer.parseInt(st.nextToken());
+		}		
+		
+		Arrays.sort(list, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				return o1[0] - o2[0];
@@ -30,35 +40,26 @@ public class BOJ12764_싸지방에간준하 {
 			}
 		}); // 끝 시간 정렬
 		PriorityQueue<Integer> labels = new PriorityQueue<>(); // 라벨 정렬
-		
-		StringTokenizer st = null;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			list.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
-		}
-		
+
 		int idx = 0; // 총 의자 개수
 		for (int i = 0; i < N; i++) {
 			
 			while(!end.isEmpty()) {
-				if(end.peek()[0] <= list.peek()[0]) {
+				if(end.peek()[0] <= list[i][0]) {
 					labels.add(end.poll()[1]);
 				} else 
 					break;
 			}
 			
 			if(labels.isEmpty()) {
-				end.add(new int[] {list.peek()[1], idx});
+				end.add(new int[] {list[i][1], idx});
 				computer[idx++]++;
 			
 			} else {
-				int label = labels.poll();
-				end.add(new int[] {list.peek()[1], label});
+				int label = labels.poll(); // 앉을 수 있는 의자 중에서 가장 작은 번호
+				end.add(new int[] {list[i][1], label});
 				computer[label]++;
 			}
-			
-			list.poll();
 		}
 		
 		StringBuilder sb = new StringBuilder();
