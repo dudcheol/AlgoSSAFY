5d7448235fa0d42589faecc52b8f78397838da67
cheonghwakim <43662673+cheonghwakim@users.noreply.github.com>
2021-01-20 18:59:40 +0900

    Update BOJ1477_휴게소세우기.java

--- 25-1주차/휴게소 세우기/김청화/BOJ1477_휴게소세우기.java
+++ 25-1주차/휴게소 세우기/김청화/BOJ1477_휴게소세우기.java
@@ -33,7 +33,7 @@ public class BOJ1477_휴게소세우기 {
 			
 			int cnt = 0;
 			for (int i = 1; i < rest.length; i++) {
-				cnt += (rest[i] - rest[i - 1] - 1) / mid; // 편의점 겹쳐서 세워지는 거 고려하기 위해 1뺌
+				cnt += (rest[i] - rest[i - 1] - 1) / mid; // 휴게소 겹쳐서 세워지는 거 고려하기 위해 1뺌
 			}
 			
 			if(cnt > M) { // M번 보다 더 많이 나눴다는 것 = mid값이 너무 작았다
